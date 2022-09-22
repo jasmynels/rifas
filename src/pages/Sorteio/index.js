98c90101c4ca6cd9d5cdd9b514fce8fa2784065a
Rafael <rafael-cristiano@live.com>
2022-09-22 19:33:13 -0300

    feat: integracao com a api e passar o id, nomes corretamente

--- src/pages/Sorteio/index.js
+++ src/pages/Sorteio/index.js
@@ -1,13 +1,37 @@
+import { useState, useEffect, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
+import api from "../../services/axios";
+
 import Header from '../../components/Header'
 import SoloPrize from '../../components/SoloPrize'
 import Footer from '../../components/Footer'
 
+export default function Sorteios() { 
+  let {id} = useParams()
+  const [rifa, setRifa] = useState()
 
-export default function Sorteios() {
+  const fetchData = useCallback(async() => {
+    const response = await fetch(`https://develop.pvrcino.codes/api/rifas/${id}`)
+    const data = await response.json()
+    if( data ){
+      setRifa(data ?? {rifa:{
+        id: '', 
+        titulo: '',
+        subtitulo: '',
+        imagem: ''
+      }})
+    }
+  }, [id])
+
+  useEffect(() => {
+    // api.get(`/rifas/${id}`).then(({ data }) => setRifa(data))
+    fetchData()
+  }, [fetchData])
+
   return (
     <div>
       <Header />
-      <SoloPrize />
+      <SoloPrize {...rifa} />
       <Footer />
     </div>
   );
